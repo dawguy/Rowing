ad2ce4d7d8c3dcd4f182b8f02b63434caed4b056
David Wright <hockeyguy411@gmail.com>
2022-04-21 13:40:33 -0400

    Revert "Attempt to use a clojure based cljs http request library."
    This reverts commit 1e5eec8b82f8080bd6d553981276da1b386ced4a.

--- rowing-client/src/main/rowing_client/browser.cljs
+++ rowing-client/src/main/rowing_client/browser.cljs
@@ -1,17 +1,13 @@
 (ns rowing-client.browser
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
-            [rowing-client.views :as views]
-            [rowing-client.cljs-http-client :as http]
-            [cljs.core.async :refer [<!]])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+            [rowing-client.views :as views]))
 
 (defn default []
   [:div "DEFAULT TEXT"])
 
 (defonce active-page (r/atom default))
 (defonce active-text (r/atom "abc"))
-(defonce db-workouts (r/atom #()))
 
 (defn aPage [] [:div "Ayoo!"])
 (defn active-text-page [] [:div @active-text])
@@ -19,8 +15,6 @@
 (defn ^:dev/after-load start []
   (rdom/render views/mainPage (js/document.getElementById "app"))
   )
-
-(go (prn (<! (http/get "http://localhost:8080/workouts/erg/1"))))
 
 (defn init []
   ;; init is called ONCE when the page loads
